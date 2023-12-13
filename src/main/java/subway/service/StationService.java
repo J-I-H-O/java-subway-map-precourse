5480d9e8ba2b6755ff66610635066a6721768709
J-I-H-O <jeongjiho0731@gmail.com>
2023-12-13 12:50:43 +0900

    feat(StationService): 중복 역 등록 여부 검증 기능 추가

--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -8,8 +8,14 @@ import java.util.List;
 
 public class StationService {
     private static final String NO_SUCH_STATION_ERROR_MESSAGE = "[ERROR] 해당 역이 존재하지 않습니다.";
+    private static final String DUPLICATE_STATION_ERROR_MESSAGE = "[ERROR] 이미 등록된 역 이름입니다.";
+
 
     public void addStation(String name) {
+        if (StationRepository.hasStation(name)) {
+            throw new IllegalArgumentException(DUPLICATE_STATION_ERROR_MESSAGE);
+        }
+
         Station station = new Station(name);
         StationRepository.addStation(station);
     }
